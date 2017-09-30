e86aa8e26a96be27705fdf20877f0c8c08fff316
Payas Awadhutkar <payasra@gmail.com>
2017-09-30 18:38:30 -0500

    Bumped up the version number to match with Atlas and added the feature project for the plugin

---
+++ L-SAP/src/com/kcsl/lsap/verifier/internal/CFGDisconnectedException.java
@@ -0,0 +1,10 @@
+package com.kcsl.lsap.verifier.internal;
+
+public class CFGDisconnectedException extends Exception {
+	
+	private static final long serialVersionUID = -5723585054892956361L;
+	
+	public CFGDisconnectedException(String string) {
+		super(string);
+	}
+}
---
+++ L-SAP/src/com/kcsl/lsap/verifier/internal/EFGCreationException.java
@@ -0,0 +1,11 @@
+package com.kcsl.lsap.verifier.internal;
+
+public class EFGCreationException extends Exception {
+
+	private static final long serialVersionUID = -8873259937325323087L;
+
+	public EFGCreationException(String string, NullPointerException e) {
+		super(string, e);
+	}
+
+}
